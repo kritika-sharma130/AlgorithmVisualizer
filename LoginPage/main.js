be4eb91e5674c1c0a0853ca9e42ec976c7be3ba4
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-25 20:46:11 +0530

    fix problem in authentication

--- LoginPage/main.js
+++ LoginPage/main.js
@@ -1,5 +1,5 @@
-
-var firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+const firebaseConfig = {
   apiKey: "AIzaSyDeDISV1UnhG8ltcQbr8yMwvvY2HKiBEQE",
   authDomain: "algovisualizer-3df04.firebaseapp.com",
   databaseURL: "https://algovisualizer-3df04-default-rtdb.firebaseio.com",
@@ -9,66 +9,73 @@ var firebaseConfig = {
   appId: "1:97030323115:web:348bec1f618f37bd6130ee",
   measurementId: "G-TPXDN3ZGEG",
 };
-
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const database = firebase.database();
+document.getElementById("registerid").addEventListener("click", register);
+document.getElementById("loginid").addEventListener("click", login);
 
 function register() {
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
-  full_name = document.getElementById("full_name").value;
-  if (validate_email(email) == false || validate_password(password) == false) {
+  console.log("register");
+  let emailId = document.getElementById("emailId").value;
+  let password = document.getElementById("password").value;
+  let full_name = document.getElementById("full_name").value;
+  if (
+    validate_email(emailId) == false ||
+    validate_password(password) == false
+  ) {
     alert("Please enter correct fields!!");
     return;
-  
   }
   if (validate_field(full_name) == false) {
     alert("Please enter the fields!!");
     return;
   }
 
-  // Move on with Auth
-  auth
-    .createUserWithEmailAndPassword(email, password)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(emailId, password)
     .then(function () {
-      
-      var user = auth.currentUser;
-      var database_ref = database.ref();
-      var user_data = {
-        email: email,
-        full_name: full_name,
-        last_login: Date.now(),
-      };
-      database_ref.child("users/" + user.uid).set(user_data);
-      alert("User Created!!");
+      let user = firebase.auth().currentUser;
+      firebase
+        .database()
+        .ref("user/" + user.uid)
+        .set({
+          name: full_name,
+          email: emailId,
+        });
+      window.location.href = "../index.html";
     })
-    .catch(function (error) {
+    .catch((e) => {
+      console.log(e);
       var error_code = error.code;
       var error_message = error.message;
       alert(error_message);
     });
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user);
+  } else {
+  }
+});
+
 function login() {
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
+  let email = document.getElementById("emailId").value;
+  let password = document.getElementById("password").value;
   if (validate_email(email) == false || validate_password(password) == false) {
     alert("Please enter correct fields!!");
     return;
   }
 
-  auth
+  firebase
+    .auth()
     .signInWithEmailAndPassword(email, password)
     .then(function () {
-      var user = auth.currentUser;
-      var database_ref = database.ref();
-      var user_data = {
-        last_login: Date.now(),
-      };
-
-      database_ref.child("users/" + user.uid).update(user_data);
-      alert("User Logged In!!");
+      console.log("hello");
+    })
+    .then(function () {
+      window.location.href = "../index.html";
     })
     .catch(function (error) {
       var error_code = error.code;
@@ -77,13 +84,9 @@ function login() {
       alert(error_message);
     });
 }
+
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/;
-  if (expression.test(email) == true) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
 
 function validate_password(password) {
@@ -98,10 +101,9 @@ function validate_field(field) {
   if (field == null) {
     return false;
   }
-
   if (field.length <= 0) {
-    return false;
     alert("Please enter fields");
+    return false;
   } else {
     return true;
   }
